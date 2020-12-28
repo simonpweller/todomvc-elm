3606db07be61c9b54c998304d6bdca1249ae0660
Simon Weller <s.meierbeck@gmail.com>
2020-12-28 11:25:25 +0100

    setup basic model and view, hide #main and #footer if there are no todos

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,81 @@
 module Main exposing (main)
 
-import Html exposing (button, div, input, label, li, section, text, ul)
-import Html.Attributes exposing (attribute, class, for, id, type_, value)
+import Browser
+import Html exposing (a, button, div, footer, input, label, li, section, span, strong, text, ul)
+import Html.Attributes exposing (attribute, class, for, href, id, type_, value)
 
 
-main : Html.Html msg
+
+-- MAIN
+
+
 main =
-    section [ class "main" ]
-        [ input [ class "toggle-all", id "toggle-all", type_ "checkbox" ]
-            []
-        , label [ for "toggle-all" ]
-            [ text "Mark all as complete" ]
-        , ul [ class "todo-list" ]
-            [ completedTodo
-            , openTodo
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+
+
+-- MODEL
+
+
+type alias Model =
+    List String
+
+
+init : Model
+init =
+    []
+
+
+
+-- VIEW
+
+
+view : Model -> Html.Html msg
+view model =
+    if List.isEmpty model then
+        text ""
+
+    else
+        div []
+            [ section
+                [ class "main" ]
+                [ input [ class "toggle-all", id "toggle-all", type_ "checkbox" ]
+                    []
+                , label [ for "toggle-all" ]
+                    [ text "Mark all as complete" ]
+                , ul [ class "todo-list" ]
+                    (List.map
+                        todo
+                        model
+                    )
+                ]
+            , footerHtml
             ]
-        ]
 
 
-completedTodo : Html.Html msg
-completedTodo =
+todo : String -> Html.Html msg
+todo todoText =
     li [ class "completed" ]
         [ div [ class "view" ]
             [ input [ attribute "checked" "", class "toggle", type_ "checkbox" ]
                 []
             , label []
-                [ text "Taste JavaScript" ]
+                [ text todoText ]
             , button [ class "destroy" ]
                 []
             ]
@@ -34,17 +84,27 @@ completedTodo =
         ]
 
 
-openTodo : Html.Html msg
-openTodo =
-    li []
-        [ div [ class "view" ]
-            [ input [ class "toggle", type_ "checkbox" ]
-                []
-            , label []
-                [ text "Buy a unicorn" ]
-            , button [ class "destroy" ]
-                []
+footerHtml =
+    footer [ class "footer" ]
+        [ span [ class "todo-count" ]
+            [ strong []
+                [ text "0" ]
+            , text "item left"
             ]
-        , input [ class "edit", value "Rule the web" ]
-            []
+        , ul [ class "filters" ]
+            [ li []
+                [ a [ class "selected", href "#/" ]
+                    [ text "All" ]
+                ]
+            , li []
+                [ a [ href "#/active" ]
+                    [ text "Active" ]
+                ]
+            , li []
+                [ a [ href "#/completed" ]
+                    [ text "Completed" ]
+                ]
+            ]
+        , button [ class "clear-completed" ]
+            [ text "Clear completed" ]
         ]
